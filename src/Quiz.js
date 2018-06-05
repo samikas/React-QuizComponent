f714b29ebe0f74410802d7486a6bab18319d41ed
samikas <samppa11@hotmail.com>
2018-06-05 11:24:46 +0300

    Quiz.js get quiz data from json

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,8 @@ class Quiz extends React.Component{
     render(){
         return(
             <div>
-                <div className="QuizQuestion">                
+                <div className="QuizQuestion">
+                {quizData.quiz_questions[0].instruction_text}                
                 </div>
             </div>
         )
