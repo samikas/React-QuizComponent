96386994e556b79478626af2a2eb30849a64248a
samikas <samppa11@hotmail.com>
2018-06-05 21:52:59 +0300

    Add quiz_question prop to Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,9 +13,7 @@ class Quiz extends Component{
     render(){
         return(
             <div>
-                <div className="QuizQuestion">
-                {quizData.quiz_questions[0].instruction_text}                
-                </div>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
             </div>
         )
     }
