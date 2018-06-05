93a42377284598f833df6b037c570773009cbdbd
samikas <samppa11@hotmail.com>
2018-06-05 17:02:54 +0300

    Create QuizQuestions class and its render method

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,2 +1,19 @@
 import React, { Component } from 'react'
 
+class QuizQuestion extends Component{
+
+    render(){
+        return(
+        <main>
+            <section>
+                <p>instruction text goes here</p>
+            </section>
+            <section className="buttons">
+                <ul>
+                    quiz question button logic goes here
+                </ul>
+            </section>
+        </main>
+        )
+    }
+}
