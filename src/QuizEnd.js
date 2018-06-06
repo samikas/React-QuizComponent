ba2b51e98abcb1a33dc358e8bf694d266b46514b
samikas <samppa11@hotmail.com>
2018-06-06 22:00:41 +0300

    Add WIP const isQuizEnd to QuizEnd

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 class QuizEnd extends Component{
     render(){
+        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
         return(
             <div>
                 <p>Thanks for playing!</p>
