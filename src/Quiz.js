b0f0b9a874b509727635f37be929d6e5132b77f2
samikas <samppa11@hotmail.com>
2018-06-13 22:35:58 +0300

    Quiz.js fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,27 +4,27 @@ import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
-class Quiz extends Component{
-    constructor(props){
-        super(props)
-        this.state={quiz_position:1}
-        
+class Quiz extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      quiz_position: 1
     }
+  }
 
-    render(){
-        //const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
-        const isQuizEnd = false
-        let result;
-        return isQuizEnd ? (
-            <div>
-              <QuizEnd />
-            </div>
-          ) : (
-            <div>
-              <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
-            </div>
-          )
-    }
+
+  render () {
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
+    return isQuizEnd ? (
+      <div>
+        <QuizEnd />
+      </div>
+    ) : (
+      <div>
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      </div>
+    )
+  }
 }
 
 export default Quiz
