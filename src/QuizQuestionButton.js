556b09559fe9600715439af2da1b1aa5802a02e2
samikas <samppa11@hotmail.com>
2018-06-14 00:09:47 +0300

    Start working on handleClick (WIP) in QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component{
+    handleClick(){
+        
+    }
     render(){
         return(
             <li>
-                <button>{this.props.button_text}</button>
+                <button onClick={}>{this.props.button_text}</button>
             </li>
         )
     }
