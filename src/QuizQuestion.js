95752b11630334d401a2be6bdf6ac92df5cbfaac
samikas <samppa11@hotmail.com>
2018-06-14 16:09:17 +0300

    Add curly braces to key in QuizQuestion

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -12,7 +12,7 @@ class QuizQuestion extends Component{
             <section className="buttons">
                 <ul>
                 {this.props.quiz_question.answer_options.map((answer_option, index) => {
-                    return <QuizQuestionButton key='index' button_text={answer_option}/>
+                    return <QuizQuestionButton key={index} button_text={answer_option}/>
                 })}                  
                 </ul>
             </section>
